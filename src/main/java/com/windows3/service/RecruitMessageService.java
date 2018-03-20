57a2aea7647cca066ca05e958ed2b5ec7ab69760
window3 <416400304@qq.com>
2018-03-20 09:39:23 +0800

    完全不知道要干啥

---
+++ src/main/java/com/windows3/service/RecruitMessageService.java
@@ -0,0 +1,14 @@
+package com.windows3.service;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+/**
+ * Created by 3 on 2018/3/20.
+ */
+
+
+public interface RecruitMessageService {
+
+}
+
---
+++ src/main/java/com/windows3/service/impl/RecruitMessageServiceImpl.java
@@ -0,0 +1,13 @@
+package com.windows3.service.impl;
+
+import com.windows3.service.RecruitMessageService;
+
+/**
+ * Created by 3 on 2018/3/20.
+ */
+
+
+public class RecruitMessageServiceImpl implements RecruitMessageService{
+
+}
+
